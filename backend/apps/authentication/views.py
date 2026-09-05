6e7527704dcad57d1b6b2d57f152af0442185915
kady-tech01 <mecharakhadidja71@gmail.com>
2026-09-05 23:27:58 +0100

    working on authentication app

--- backend/apps/authentication/views.py
+++ backend/apps/authentication/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer, UserProfileSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    """
+    إنشاء حساب جديد لـ مستخدم المنصة
+    """
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    """
+    عرض وتحديث بيانات الملف الشخصي للمستخدم المسجل
+    """
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
